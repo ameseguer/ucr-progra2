131ceda420ef1eb537cf17e4738cea8b08e382ee
ameseguer <andres.meseguer@ucr.ac.cr>
2018-03-20 16:37:47 -0600

    Semana 2

---
+++ org.eclipse.papyrus.javagen.Calle/src/TestCalle/TestCarro.java
@@ -0,0 +1,29 @@
+package TestCalle;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import Calle.Calle.Carro;
+
+
+class TestCarro {
+	
+	static Carro  carroDePrueba;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		carroDePrueba = new Carro();
+	}
+
+	@DisplayName("Probar getVelocidad después de setVelocidad")
+	@Test
+	void testSetGetVelocidad() {
+		int valorPrueba= 100;
+		carroDePrueba.setVelocidad(valorPrueba);
+		int retorno = carroDePrueba.getVelocidad();
+		assertEquals(valorPrueba, retorno);
+	}
+
+}
