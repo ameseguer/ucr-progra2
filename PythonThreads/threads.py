2d3c01e37fa924aac4318b843160d701ebcf03ed
ameseguer <ameseguer@gmail.com>
2018-05-31 17:45:08 -0600

    Ejemplos de threads en Python
    Ranking de animales

--- PythonThreads/threads.py
+++ PythonThreads/threads.py
@@ -6,10 +6,12 @@ import random
 ARGS & VARS
 """
 laps=10
-delay=0.3
+delay=0.06
 sleepTime=3
 threads = []
 waitLocks= {}
+ranking=[]
+rankLock=threading.Condition();
 
 for lock in range(laps):
     waitLocks[lock]=threading.Condition()
@@ -22,9 +24,12 @@ def turtle(laps,delay):
     for counter in range(laps):
         print threading.currentThread().getName()+"["+str(counter)+"]"
         time.sleep(delay);
-        waitLocks[lock].acquire()
-        waitLocks[lock].notifyAll()
-        waitLocks[lock].release()
+        waitLocks[counter].acquire()
+        waitLocks[counter].notifyAll()
+        waitLocks[counter].release()
+    rankLock.acquire()
+    ranking.append(threading.currentThread().getName())
+    rankLock.release()
     return
 """
 HARE
@@ -36,10 +41,9 @@ def hare(laps,sleepTime):
         randomDice=random.randint(1, 100)
         if slept == 0 and randomDice > 80 :
             print threading.currentThread().getName()+"[zzzZZZZ]"
-            '''time.sleep(sleepTime)'''
-            waitLocks[lock].acquire()
-            sleepQuality=waitLocks[lock].wait(sleepTime)
-            waitLocks[lock].release()
+            waitLocks[counter].acquire()
+            sleepQuality=waitLocks[counter].wait(sleepTime)
+            waitLocks[counter].release()
             slept=1
 
             if not sleepQuality :
@@ -47,6 +51,9 @@ def hare(laps,sleepTime):
             else:
                 hareStatus="God morning sunshine"
             print threading.currentThread().getName()+"["+hareStatus+"]"
+    rankLock.acquire()
+    ranking.append(threading.currentThread().getName())
+    rankLock.release()
     return
 
 """
@@ -57,6 +64,7 @@ MAIN LOOPS
 for i in range(5):
     turtleName="turtle"+str(i)
     t = threading.Thread(name=turtleName,target=turtle,args=(laps,delay))
+    threads.append(t)
     t.start()
 
 for i in range(5):
@@ -64,3 +72,13 @@ for i in range(5):
     t = threading.Thread(name=hareName,target=hare,args=(laps,sleepTime))
     threads.append(t)
     t.start()
+
+"""Polling"""
+for animalThread in threads :
+    animalThread.join()
+
+counter=0
+print  "RANKING"
+for animal in ranking:
+    print "#",counter,animal
+    counter+=1
